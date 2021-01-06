a2fa57aa2ff0e447152fdb811868adf28e5a92d8
Caleb Fahlgren <calebfahlgren@protonmail.com>
2021-01-06 18:17:27 -0500

    added sentry error reporting / monitoring

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const slackRouter = require("./src/routes/slack"); // import routes for slack slash command
 const indexRouter = require("./src/routes/index"); // import routes for index
+const Sentry = require('@sentry/node');
+const Tracing = require("@sentry/tracing");
 
 const app = express();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 // use body parser to parse url-encoded form data
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +32,8 @@ app.get("/*", indexRouter.index);
 
 // slack slash command endpoint
 app.post("/slack", slackRouter.slackResponse);
+
+app.use(Sentry.Handlers.errorHandler());
 
 // Error handler
 app.use((err, req, res) => {
